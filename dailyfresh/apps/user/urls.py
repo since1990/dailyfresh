e695ff505f1139668e31161505c6891907e8243b
since1990 <353622522@qq.com>
2019-02-19 11:16:01 +0800

    注册页面

--- dailyfresh/apps/user/urls.py
+++ dailyfresh/apps/user/urls.py
@@ -3,5 +3,6 @@ from user import views
 
 
 urlpatterns = [
-    url(r'^register$', views.register, name='register')
+    url(r'^register$', views.register, name='register'),
+    url(r'^register_handle$', views.register_handle, name='register_handle'),
 ]
--- dailyfresh/apps/user/views.py
+++ dailyfresh/apps/user/views.py
@@ -1,7 +1,70 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
+from user.models import User
 
 
 # Create your views here.
 def register(request):
-    '''注册页面'''
-    return render(request, 'register.html')
+    '''注册'''
+    if request.method == 'GET':
+        # 显示注册页面
+        return render(request, 'register.html')
+    else:
+        # 进行注册处理
+        # 接收数据
+        username = request.POST.get('user_name')
+        password = request.POST.get('pwd')
+        email = request.POST.get('email')
+
+        # 数据校验
+        if not all([username, password, email]):
+            return render(request, 'register.html', {'errmsg': '数据不完整'})
+        # 校验用户名是否重复
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            # 说明用户名不存在
+            user = None
+
+        if user:
+            # 满足条件说明用户名已存在
+            return render(request, 'register.html', {'errmsg': '用户名已存在'})
+
+        # 业务处理
+        user = User.objects.create_user(username, email, password)
+        user.is_active = 0
+        user.save()
+
+        # 返回应答，跳转到首页
+        return redirect(reverse('goods:index'))
+
+
+'''
+def register_handle(request):
+    # 接收数据
+    username = request.POST.get('user_name')
+    password = request.POST.get('pwd')
+    email = request.POST.get('email')
+
+    # 数据校验
+    if not all([username, password, email]):
+        return render(request, 'register.html', {'errmsg': '数据不完整'})
+    # 校验用户名是否重复
+    try:
+        user = User.objects.get(username=username)
+    except User.DoesNotExist:
+        # 说明用户名不存在
+        user = None
+
+    if user:
+        # 满足条件说明用户名已存在
+        return render(request, 'register.html', {'errmsg':'用户名已存在'})
+
+    # 业务处理
+    user = User.objects.create_user(username, email, password)
+    user.is_active = 0
+    user.save()
+
+    # 返回应答，跳转到首页
+    return redirect(reverse('goods:index'))
+'''
