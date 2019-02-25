8aa26c8253cea463640b6c3ed234c80d37f9ad4b
since1990 <353622522@qq.com>
2019-02-25 18:54:21 +0800

    nginx配置

--- dailyfresh/apps/goods/admin.py
+++ dailyfresh/apps/goods/admin.py
@@ -1,11 +1,37 @@
 from django.contrib import admin
-from goods.models import GoodsType, IndexGoodsBanner, IndexTypeGoodsBanner, GoodsSKU, Goods, IndexPromotionBanner
+from goods.models import GoodsType, IndexGoodsBanner, IndexTypeGoodsBanner
+from goods.models import GoodsSKU, Goods, IndexPromotionBanner, GoodsImage
+from django.core.cache import cache
+
+
+class BaseModelAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        '''新增或更新表中的数据时调用'''
+        super().save_model(request, obj, form, change)
+
+        # 发出任务，让celery worker重新生成首页静态页
+        from celery_tasks.tasks import generate_static_index_html
+        generate_static_index_html.delay()
+
+        # 清除首页的缓存数据
+        cache.delete('index_page_data')
+
+    def delete_model(self, request, obj):
+        '''删除表中的数据时调用'''
+        super().delete_model(request, obj)
+        # 发出任务，让celery worker重新生成首页静态页
+        from celery_tasks.tasks import generate_static_index_html
+        generate_static_index_html.delay()
+
+        # 清除首页的缓存数据
+        cache.delete('index_page_data')
 
 
 # Register your models here.
-admin.site.register(Goods)
-admin.site.register(GoodsType)
-admin.site.register(GoodsSKU)
-admin.site.register(IndexGoodsBanner)
-admin.site.register(IndexTypeGoodsBanner)
-admin.site.register(IndexPromotionBanner)
+admin.site.register(GoodsImage, BaseModelAdmin)
+admin.site.register(Goods, BaseModelAdmin)
+admin.site.register(GoodsType, BaseModelAdmin)
+admin.site.register(GoodsSKU, BaseModelAdmin)
+admin.site.register(IndexGoodsBanner, BaseModelAdmin)
+admin.site.register(IndexTypeGoodsBanner, BaseModelAdmin)
+admin.site.register(IndexPromotionBanner, BaseModelAdmin)
