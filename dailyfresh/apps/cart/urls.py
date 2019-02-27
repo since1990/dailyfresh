d850879abadab38d127b1955e3ce255a65203a0e
since1990 <353622522@qq.com>
2019-02-27 18:09:38 +0800

    购物车页面全选全不选功能

--- dailyfresh/apps/cart/urls.py
+++ dailyfresh/apps/cart/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from cart.views import CartAddView
+from cart.views import CartAddView, CartInfoView
 
 
 urlpatterns = [
     url(r'^add$', CartAddView.as_view(), name='add'),
+    url(r'^$', CartInfoView.as_view(), name='show'),
 ]
--- dailyfresh/apps/cart/views.py
+++ dailyfresh/apps/cart/views.py
@@ -3,6 +3,7 @@ from django.views.generic import View
 from django.http import JsonResponse
 from goods.models import GoodsSKU
 from django_redis import get_redis_connection
+from utils.mixin import LoginRequiredMixin
 # Create your views here.
 
 
@@ -63,3 +64,45 @@ class CartAddView(View):
 
         # 返回应答
         return JsonResponse({'res': 5, 'total_count': total_count, 'message': '添加成功'})
+
+
+class CartInfoView(LoginRequiredMixin, View):
+    '''购物车页面'''
+    def get(self, request):
+        # 获取登录的用户
+        user = request.user
+
+        # 获取用户购物车中商品的信息
+        conn = get_redis_connection('default')
+        cart_key = 'cart_%d' % user.id
+        # {'商品id':商品数量}
+        cart_dict = conn.hgetall(cart_key)
+
+        # 商品表
+        skus = []
+        # 保存用户购物车中商品的总数目和总价格
+        total_count = 0
+        total_price = 0
+
+        # 遍历获取商品的信息
+        for sku_id, count in cart_dict.items():
+            # 根据商品的id获取商品的信息
+            sku = GoodsSKU.objects.get(id=sku_id)
+            # 计算商品的小计
+            amount = sku.price * int(count)
+            # 动态给sku对象增加一个属性amount, 保存商品的小计
+            sku.amount = amount
+            # 动态给sku对象增加一个属性count, 保存购物车中对应商品的数量
+            sku.count = count
+            # 往商品表中添加数据
+            skus.append(sku)
+            # 计算商品的总数，总价格
+            total_count += int(count)
+            total_price += amount
+
+        # 组织上下文
+        context = {'total_count': total_count,
+                   'total_price': total_price,
+                   'skus': skus}
+
+        return render(request, 'cart.html', context)
