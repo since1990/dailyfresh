59435ee073d661ba9222a32adbca083878b0cccc
since1990 <353622522@qq.com>
2019-02-28 11:46:01 +0800

    购物车页面完善

--- dailyfresh/apps/cart/views.py
+++ dailyfresh/apps/cart/views.py
@@ -151,6 +151,40 @@ class CartUpdateView(View):
             total_count += int(val)
 
         # 返回应答
-        return JsonResponse({'res': 5, 'total_count': total_count,'message': '更新成功'})
+        return JsonResponse({'res': 5, 'total_count': total_count, 'message': '更新成功'})
 
 
+class CartDeleteView(View):
+    '''购物车记录删除'''
+    def post(self, request):
+        # 判断用户是否登陆
+        user = request.user
+        if not user.is_authenticated():
+            return JsonResponse({'res': 0, 'errmsg': '请先登陆'})
+
+        # 接收数据
+        sku_id = request.POST.get('sku_id')
+        # 校验数据
+        if not sku_id:
+            return JsonResponse({'res': 1, 'errmsg': '无效数据'})
+
+        # 校验商品是否存在
+        try:
+            sku = GoodsSKU.objects.get(id=sku_id)
+        except GoodsSKU.DoesNotExist:
+            # 商品不存在
+            return JsonResponse({'res': 2, 'errmsg': '商品不存在'})
+
+        # 业务处理
+        conn = get_redis_connection('default')
+        cart_key = 'cart_%d' % user.id
+        conn.hdel(cart_key, sku_id)
+
+        # 计算购物车中商品的总件数
+        total_count = 0
+        vals = conn.hvals(cart_key)
+        for val in vals:
+            total_count += int(val)
+
+        # 返回应答
+        return JsonResponse({'res': 3, 'total_count': total_count, 'message': '删除成功'})
